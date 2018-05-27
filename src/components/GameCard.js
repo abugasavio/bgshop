1ee533cbd1c7d8d515b4bdf3d6325932a7d4705c
Savio Abuga <savioabuga@gmail.com>
2018-05-27 15:30:41 +0300

    Fix: add some missing tags to the GameCard component

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const GameCard = function () {
-    return (
-        <div className="ui card">
-            <div className="image">
-                <img
-                    src="https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
-                    alt=""/>
-            </div>
-            <div className="content">
-                <div className="meta">
-                    <i className="icon user"/>2-4&nbsp;
-                    <i className="icon user"/>60 min
-                </div>
+const GameCard = () => (
+    <div className="ui card">
+        <div className="image">
+            <span className="ui green ribbon label">90.00</span>
+            <img
+                src="https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+                alt="Very Nice Image"/>
+        </div>
+        <div className="content">
+            <a href="#" className="header">Quadropolis</a>
+            <div className="meta">
+                <i className="icon user"/>2-4&nbsp;
+                <i className="icon user"/>60 min
             </div>
         </div>
-    )
-}
+    </div>
+)
 
 export default GameCard
