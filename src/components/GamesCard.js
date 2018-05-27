3466a4e9988522afc5139444f0046b5526549003
Savio Abuga <savioabuga@gmail.com>
2018-05-27 16:21:37 +0300

    Using prop-types

--- src/components/GamesCard.js
+++ src/components/GamesCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const GamesCard = ({game}) => (
     <div className="ui card">
         <div className="image">
-            <span className="ui green ribbon label">{game.price}</span>
+            <span className="ui green ribbon label">${game.price}</span>
             <img src={game.thumbnail} alt="Very Nice Image"/>
         </div>
         <div className="content">
@@ -16,5 +17,11 @@ const GamesCard = ({game}) => (
         </div>
     </div>
 )
+
+GamesCard.propTypes = {
+    game: PropTypes
+        .shape({price: PropTypes.string.isRequired, name: PropTypes.string.isRequired, players: PropTypes.string.isRequired, duration: PropTypes.string.isRequired})
+        .isRequired
+}
 
 export default GamesCard
--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GamesCard from './GamesCard';
+import PropTypes from 'prop-types';
 
 const GamesList = ({games}) => {
     return (
@@ -7,6 +8,12 @@ const GamesList = ({games}) => {
             {games.map(game => <GamesCard game={game} key={game._id}/>)}
         </div>
     );
+};
+
+GamesList.propTypes = {
+    games: PropTypes
+        .arrayOf(PropTypes.object)
+        .isRequired
 };
 
 export default GamesList;
