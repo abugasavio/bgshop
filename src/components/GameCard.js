4048a67d4508e9bd58857dcb8c459e5ea5860c0b
Savio Abuga <savioabuga@gmail.com>
2018-05-27 15:41:12 +0300

    Add dynamic content to GamesCard component

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const GameCard = () => (
+const GameCard = ({game}) => (
     <div className="ui card">
         <div className="image">
-            <span className="ui green ribbon label">90.00</span>
-            <img
-                src="https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
-                alt="Very Nice Image"/>
+            <span className="ui green ribbon label">{game.price}</span>
+            <img src={game.thumbnail} alt="Very Nice Image"/>
         </div>
         <div className="content">
-            <a href="#" className="header">Quadropolis</a>
+            <a href="#" className="header">{game.name}</a>
             <div className="meta">
-                <i className="icon user"/>2-4&nbsp;
-                <i className="icon user"/>60 min
+                <i className="icon user"/>{game.players}&nbsp;
+                <i className="icon user"/>{game.duration}
+                min
             </div>
         </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@ import {render} from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import GameCard from './components/GameCard';
 
+const games = [
+    {
+        "name": "Roll for the Galaxy",
+        "thumbnail": "https://images.pexels.com/photos/205324/pexels-photo-205324.jpeg?auto=compress&c" +
+                "s=tinysrgb&dpr=2&h=750&w=1260",
+        "price": "29.99",
+        "players": "2-5",
+        "duration": "45"
+    }, {
+        "name": "Roll for the Galaxy",
+        "thumbnail": "https://images.pexels.com/photos/205324/pexels-photo-205324.jpeg?auto=compress&c" +
+                "s=tinysrgb&dpr=2&h=750&w=1260",
+        "price": "29.99",
+        "players": "2-5",
+        "duration": "45"
+    }, {
+        "name": "Roll for the Galaxy",
+        "thumbnail": "https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&c" +
+                "s=tinysrgb&h=750&w=1260",
+        "price": "29.99",
+        "players": "2-5",
+        "duration": "45"
+    }
+
+]
+
 render(
-    <GameCard/>, document.getElementById('root'));
+    <GameCard game={games[2]}/>, document.getElementById('root'));
