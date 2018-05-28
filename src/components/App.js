fc5e15e9579ac53ed15a277da8a1348a659c1ce8
Savio Abuga <savioabuga@gmail.com>
2018-05-28 16:01:40 +0300

    Add toggling of image and descriptions

--- src/components/App.js
+++ src/components/App.js
@@ -5,31 +5,50 @@ import _orderBy from 'lodash/orderBy';
 const games = [
     {
         "_id": 1,
-        "name": "Roll for the Galaxy",
+        "name": "Milele",
         "thumbnail": "https://images.pexels.com/photos/45775/pexels-photo-45775.jpeg?auto=compress&cs=" +
                 "tinysrgb&dpr=2&h=750&w=1260",
         "price": "29.99",
         "players": "2-5",
         "duration": "45",
-        "featured": false
+        "featured": false,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i" +
+                "ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru" +
+                "d exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aut" +
+                "e irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat n" +
+                "ulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui" +
+                " officia deserunt mollit anim id est laborum."
     }, {
         "_id": 2,
-        "name": "ARoll for the Galaxy",
+        "name": "Superman",
         "thumbnail": "https://images.pexels.com/photos/205324/pexels-photo-205324.jpeg?auto=compress&c" +
                 "s=tinysrgb&dpr=2&h=750&w=1260",
         "price": "29.99",
         "players": "2-5",
         "duration": "45",
-        "featured": false
+        "featured": false,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i" +
+                "ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru" +
+                "d exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aut" +
+                "e irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat n" +
+                "ulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui" +
+                " officia deserunt mollit anim id est laborum."
     }, {
         "_id": 3,
-        "name": "Roll for the Galaxy",
+        "name": "Kila kitu",
         "thumbnail": "https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&c" +
                 "s=tinysrgb&h=750&w=1260",
         "price": "29.99",
         "players": "2-5",
         "duration": "45",
-        "featured": true
+        "featured": true,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i" +
+                "ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru" +
+                "d exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aut" +
+                "e irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat n" +
+                "ulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui" +
+                " officia deserunt mollit anim id est laborum."
+
     }
 
 ]
--- src/components/GamesCard.js
+++ src/components/GamesCard.js
@@ -3,30 +3,57 @@ import PropTypes from 'prop-types';
 import Price from './Price';
 import Featured from './Featured';
 
-const GamesCard = ({game, toggleFeatured}) => (
-    <div className="ui card">
-        <Featured
-            featured={game.featured}
-            toggleFeatured={toggleFeatured}
-            gameId={game._id}/>
-        <div className="image">
-            <Price price={game.price}/>
-            <img src={game.thumbnail} alt="Very Nice Image"/>
-        </div>
-        <div className="content">
-            <a href="#" className="header">{game.name}</a>
-            <div className="meta">
-                <i className="icon user"/>{game.players}&nbsp;
-                <i className="icon user"/>{game.duration}
-                min
+class GamesCard extends React.Component {
+    state = {
+        showImage: true
+    }
+
+    toggleShowImage = () => this.setState({
+        showImage: !this.state.showImage
+    })
+
+    render() {
+        const {game, toggleFeatured} = this.props;
+        return (
+            <div className="ui card">
+                <Featured
+                    featured={game.featured}
+                    toggleFeatured={toggleFeatured}
+                    gameId={game._id}/>
+                <div className="image">
+                    <Price price={game.price}/> {this.state.showImage
+                        ? <img src={game.thumbnail} alt="Very Nice Image"/>
+                        : <div className="description">
+                            <p>{game.description}</p>
+                        </div>}
+
+                </div>
+                <div className="content">
+                    <a href="#" className="header">{game.name}</a>
+                    <div className="meta">
+                        <i className="icon user"/>{game.players}&nbsp;
+                        <i className="icon user"/>{game.duration}
+                        min
+                        <a className="right floated" onClick={this.toggleShowImage}>
+                            <i className="icon eye"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 GamesCard.propTypes = {
     game: PropTypes
-        .shape({price: PropTypes.string.isRequired, name: PropTypes.string.isRequired, players: PropTypes.string.isRequired, duration: PropTypes.string.isRequired, featured: PropTypes.string.isRequired})
+        .shape({
+        price: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        players: PropTypes.string.isRequired,
+        duration: PropTypes.string.isRequired,
+        featured: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired
+    })
         .isRequired,
     toggleFeatured: PropTypes.func.isRequired
 }
