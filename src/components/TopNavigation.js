c12dcc7423f44e5a4fb805b1c94d331da1705b23
Savio Abuga <savioabuga@gmail.com>
2018-06-07 12:53:33 +0300

    Add react routes

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {NavLink} from 'react-router-dom';
 
-const TopNavigation = ({showGameForm, showLoginForm, showSignupForm}) => (
-    <div className="ui secondary pointing menu">
-        <a href="/" className="item">BGShop</a>
-        <a className="item" onClick={showGameForm}>
-            <i className="icon plus" />Add New Game
-        </a>
-        <a className="item" onClick={showLoginForm}>
-            <i className="icon plus" />Login
-        </a>
-        <a className="item" onClick={showSignupForm}>
-            <i className="icon plus" />Signup
-        </a>
-    </div>
+const TopNavigation = () => (
+  <div className="ui secondary pointing menu">
+    <NavLink exact to="/" className="item">BGShop</NavLink>
+    <NavLink exact className="item" to="/games">
+      Games
+    </NavLink>
+    <NavLink exact className="item" to="/games/new">
+      <i className="icon plus"/>Add New Game
+    </NavLink>
+  </div>
 )
-
-TopNavigation.propTypes = {
-    showGameForm: PropTypes.func.isRequired,
-    showLoginForm: PropTypes.func.isRequired,
-    showSignupForm: PropTypes.func.isRequired
-};
 
 export default TopNavigation
